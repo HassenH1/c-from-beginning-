c8a6b2a103ac48c0308511153de0c8f21d5e3c78
HassenH1 <hasansaid51@gmail.com>
2020-04-15 20:12:59 -0700

    functions params

---
+++ functionParam.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void showMenu() //function defined, void means doesnt return anything
+{
+  cout << "1. Search " << endl;
+  cout << "2. View Record " << endl;
+  cout << "3. Quit" << endl;
+};
+
+int getInput() //return integar
+{
+  cout << "Enter selection" << endl;
+  int input;
+  cin >> input;
+  return input;
+};
+
+void processSelection(int input)
+{
+  switch (input)
+  {
+  case 1:
+    cout << "Searching..." << endl;
+    break;
+  case 2:
+    cout << "Viewing..." << endl;
+    break;
+  case 3:
+    cout << "Quitting" << endl;
+    break;
+  default:
+    cout << "Please select an item from the menu.." << endl;
+  }
+};
+
+int main()
+{
+  showMenu();
+  int input = getInput();
+  processSelection(input);
+
+  return 0;
+}
